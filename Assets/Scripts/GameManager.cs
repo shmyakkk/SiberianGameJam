c61be498a6ddb0e8b770a77046f86ba55861b65c
shmyakk <m2008775@edu.misis.ru>
2022-05-01 11:16:27 +0300

    Изменение света

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,29 +7,14 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI timer;
 
-    private int dayTime = 10;
-    private int nightTime = 10;
-
-    public int DayTime { get => dayTime; }
-    public int NightTime { get => nightTime; }
+    public int DayTime { get; } = 10;
+    public int NightTime { get; } = 10;
 
     private void Start()
     {
-        GlobalEventManager.OnStartedDay.AddListener(StartDay);
-        GlobalEventManager.OnStartedNight.AddListener(StartNight);
         StartCoroutine(DayTimer(DayTime));
-        GlobalEventManager.SendStartedDay();
     }
 
-    private void StartDay()
-    {
-        Debug.Log("Start Day");
-    }
-
-    private void StartNight()
-    {
-        Debug.Log("Start Night");
-    }
 
     private IEnumerator DayTimer(int time)
     {
@@ -57,6 +42,7 @@ public class GameManager : MonoBehaviour
         {
             timer.text = "";
             StartCoroutine(DayTimer(DayTime));
+            
             GlobalEventManager.SendStartedDay();
         }
     }
