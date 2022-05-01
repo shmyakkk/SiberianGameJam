251f58a2e70989dfd525efd76e03808cd55fb5b0
shmyakk <m2008775@edu.misis.ru>
2022-05-02 00:13:53 +0300

    бросок Q

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem bangPS;
+    private Rigidbody bombRB;
+    private Vector3 throwDirection;
+    private float force;
+
+    public float Force { get => force; set => force = value; }
+
+    private void Start()
+    {
+        bombRB = GetComponent<Rigidbody>();
+        throwDirection = new Vector3(1, 1, 0);
+
+        bombRB.AddForce(throwDirection * Force, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var bang = Instantiate(bangPS, gameObject.transform.position, bangPS.transform.rotation);
+        bang.Play();
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 3;
+    [SerializeField] private GameObject bomb;
 
     private Vector3 inputX;
     private Vector3 inputY;
 
-    private bool isStair = false;
+    private float startTime = 0;
+    private float holdTime = 0;
 
+    private bool isStair = false;
+    private void Update() => ThrowBomb();
     private void FixedUpdate()
     {
         PlayerMoveX();
@@ -28,6 +32,26 @@ public class PlayerController : MonoBehaviour
         gameObject.transform.Translate(speed * Time.deltaTime * inputY);
     }
 
+    private void ThrowBomb()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            startTime = Time.time;
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            holdTime = Time.time - startTime;
+
+            var force = holdTime * 12;
+            if (force > 10) force = 10;
+
+            var currentBomb = Instantiate(bomb, gameObject.transform.position + Vector3.right, bomb.transform.rotation);
+            currentBomb.GetComponent<Bomb>().Force = force;
+
+            startTime = 0;
+            holdTime = 0;
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
