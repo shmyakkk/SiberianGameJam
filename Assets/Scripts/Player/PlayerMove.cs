cf383db3b9d424edb947267424211c4b574ea0bb
shmyakk <m2008775@edu.misis.ru>
2022-05-02 20:48:42 +0300

    Движение перса

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,54 +4,60 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField] private float speed = 3f;
     private Rigidbody playerRB;
-    private float speed = 3f;
 
     private bool isStair = false;
+    private bool useStair = false;
+    private Vector3 stairPos;
 
     private void Start()
     {
-        playerRB = GetComponent<Rigidbody>();   
+        playerRB = GetComponent<Rigidbody>();
     }
     private void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = 0;
+        float inputX = useStair ? 0 : Input.GetAxis("Horizontal");
 
-        if (isStair)
-        {
-            inputY = Input.GetAxis("Vertical");
-        }
+        float inputY = 0;
+        if (isStair) inputY = Input.GetAxis("Vertical");
 
         Vector3 directionVector = new Vector3(inputX, inputY, 0);
         Vector3 rotationVector = new Vector3(-inputX, 0, 0);
 
-        if (inputY != 0)
+        if (inputY != 0 && isStair)
         {
             rotationVector = Vector3.back;
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(stairPos.x, transform.position.y, transform.position.z), 3 * Time.deltaTime);
         }
 
         if (directionVector.magnitude > Mathf.Abs(0.1f))
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotationVector), Time.deltaTime * 10);
 
-        playerRB.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+        playerRB.velocity = directionVector * speed;
     }
 
     private void OnTriggerStay(Collider other)
     {
+        if (other.CompareTag("Ground")) useStair = false;
+
         if (other.CompareTag("Stair"))
         {
             isStair = true;
-            GetComponent<Rigidbody>().useGravity = false;
+            playerRB.useGravity = false;
+
+            stairPos = other.transform.position;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.CompareTag("Ground")) useStair = true;
+
         if (other.CompareTag("Stair"))
         {
             isStair = false;
-            GetComponent<Rigidbody>().useGravity = true;
+            playerRB.useGravity = true;
         }
     }
 }
